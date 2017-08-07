7b9a488478dc2a95b699431e1451737ea76e8d45
Raymond Chung <raymond_chung2@hotmail.com>
2017-08-07 17:59:01 -0400

    Learned how to put it in a table... a lot more simpler than I thought

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-//Registering the component
-Vue.component('pokedex', {
-	props: ['todo'],
-	template: '<li>{{ todo.name }}</li>'
-})
-
 var app1 = new Vue({
 	el: '#app1',
 	data: {
 		pokemonList: [
-			{ id: 1, name: 'Bulbasaur'},
-			{ id: 2, name: 'Ivysaur' },
-      		{ id: 3, name: 'Venusaur' },
-      		{ id: 4, name: 'Charmander' },
-      		{ id: 5, name: 'Charmeleon'},
-      		{ id: 6, name: 'Charizard'},
-      		{ id: 7, name: 'Squirtle'},
-      		{ id: 8, name: 'Wartortle'},
-      		{ id: 9, name: 'Blastoise'}
+			{ id: 1, name: 'Bulbasaur', type: 'Grass/Poison' },
+			{ id: 2, name: 'Ivysaur', type: 'Grass/Poison' },
+      		{ id: 3, name: 'Venusaur', type: 'Grass/Poison' },
+      		{ id: 4, name: 'Charmander', type: 'Fire' },
+      		{ id: 5, name: 'Charmeleon', type: 'Fire' },
+      		{ id: 6, name: 'Charizard', type: 'Fire/Flying' },
+      		{ id: 7, name: 'Squirtle', type: 'Water' },
+      		{ id: 8, name: 'Wartortle', type: 'Water' },
+      		{ id: 9, name: 'Blastoise', type: 'Water' }
 		]
 	}
 })
